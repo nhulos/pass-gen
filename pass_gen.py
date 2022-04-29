f24f9e024105be41d26132346ac34433279bda5f
nhulos <104386385+nhulos@users.noreply.github.com>
2022-04-29 19:07:21 +0000

    Add files via upload

--- pass_gen.py
+++ pass_gen.py
@@ -19,6 +19,28 @@ def input_num(input_text):
         print("Incorrect!")
 
 
+# answer yes/no
+def answer(text, default):
+    result = input(text).strip().lower()
+    if result == "":
+        return default
+    elif result == "y":
+        return True
+    else:
+        return False
+
+
+# randomization some numbers
+def rand_one_int(numbers_list):
+    result = []
+    for num in numbers_list:
+        num = str(num)
+        for random_num in range(10):
+            for i in range(len(num)):
+                result.append(num[:i] + str(random_num) + num[i+1:])
+    return result
+
+
 # passwords category
 def processing(data_list):
     text = []
@@ -27,58 +49,53 @@ def processing(data_list):
     for data in data_list:
         # number
         if data.isdigit():
-            for i in range(1, len(data) + 1):
-                numbers.append(data[:i])
-            for i in range(1, len(data)):
-                numbers.append(data[i:])
+            if len(data) > 4:
+                for i in range(1, len(data) + 1):
+                    numbers.append(data[:i])
+                for i in range(1, len(data)):
+                    numbers.append(data[i:])
+            else:
+                numbers.append(data)
         # data
         elif re.findall(r"\d\d-\d\d-\d\d\d\d", data):
             numbers = numbers + data.split("-")
             numbers.append(data[-2:])
         # name
         else:
-            text.append(data)
-            text.append(data.lower())
-            text.append(data.upper())
-            text.append(data.title())
+            data_types = (data, data.lower(), data.upper(), data.title())
+            for word in data_types:
+                text.append(word)
             for i in range(3, len(data)):
-                strip_data = data[:i]
+                text.append(data[:i])
+
     return text, numbers
 
 
 # generate combinations
-def generate_pass(text, numbers, min_len, max_len):
+def generate_pass(text, numbers, min_len, max_len, num_and_num):
     separators = ["", "_", "-", " "]
-    passwords = []
 
     # add passwords to list
-    def add_pass(password):
-        if not password in passwords and min_len <= len(password) <= max_len:
-            passwords.append(password)
+    def write_pass(password):
+        if min_len <= len(password) <= max_len:
+            with open("pass.txt", "a") as f:
+                f.write(password + "\n")
 
     for sep in separators:
         for t in text:
             # text + number
             for n in numbers:
-                add_pass(t + sep + n)
-                add_pass(n + sep + t)
+                write_pass(t + sep + n)
+                write_pass(n + sep + t)
             # text + text
             for t2 in text:
-                add_pass(t + sep + t2)
+                write_pass(t + sep + t2)
 
         # number + number
-        for n in numbers:
-            for n2 in numbers:
-                add_pass(n + sep + n2)
-
-    return passwords
-
-
-# write passwords to file
-def write_pass(passwords):
-    with open("pass.txt", "w") as f:
-        for i in passwords:
-            f.write(i + "\n")
+        if num_and_num:
+            for n in numbers:
+                for n2 in numbers:
+                    write_pass(n + sep + n2)
 
 
 def main():
@@ -91,15 +108,29 @@ def main():
             min_len = input_num("min len: ")
             max_len = input_num("max len: ")
             text, numbers = processing(data_list)  # categorys
+            # random one number
+            if answer("Generate combinations with one random number replace[y/N]: ", False):
+                numbers += rand_one_int(numbers)
+            # num + num
+            if answer("Generate num + num combinations[y/N]: ", False):
+                num_and_num = True
+            else:
+                num_and_num = False
             print("\nGenerating...")
-            passwords = generate_pass(
-                text, numbers, min_len, max_len)  # generate
-            write_pass(passwords)  # write to file
-            print(len(passwords), "passwords was generated!")
+            # generate
+            generate_pass(text, numbers, min_len, max_len, num_and_num)
+            print(sum(1 for line in open('pass.txt', 'r')),
+                  "passwords was generated!")
             break
         # input data
         if input_data:
             data_list.append(input_data)
+
+
+# clear pass.txt
+def clear_file():
+    with open("pass.txt", "w") as f:
+        pass
 
 
 if __name__ == "__main__":
